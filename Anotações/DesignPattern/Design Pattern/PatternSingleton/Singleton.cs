8ce981d60cd657e0806b0754f48f1a5a601697c4
Hugo André <66370123+HugoAndreL@users.noreply.github.com>
2023-11-27 12:26:51 -0300

    27/11/2023
    Atualizando o design pattern com o padrão singleton e opção de escolha

---
+++ Anotações/DesignPattern/Design Pattern/PatternSingleton/Singleton.cs
@@ -0,0 +1,45 @@
+﻿namespace Design_Pattern.PatternSingleton
+{
+    /*
+     * O padrão Singleton resolve dois problemas de uma só vez, violando o princípio de responsabilidade única:
+     * 
+     * 1. Garantir que uma classe tenha apenas uma única instância. Por que alguém iria querer controlar quantas instâncias uma classe tem? A razão mais comum para isso é para controlar o acesso a algum recurso compartilhado—por exemplo, uma base de dados ou um arquivo.
+     * Funciona assim: imagine que você criou um objeto, mas depois de um tempo você decidiu criar um novo. Ao invés de receber um objeto fresco, você obterá um que já foi criado.
+     * Observe que esse comportamento é impossível implementar com um construtor regular uma vez que uma chamada do construtor deve sempre retornar um novo objeto por design.
+     * 
+     * 2. Fornece um ponto de acesso global para aquela instância. Se lembra daquelas variáveis globais que você (tá bom, eu) usou para guardar alguns objetos essenciais? Embora sejam muito úteis, elas também são muito inseguras uma vez que qualquer código pode potencialmente sobrescrever os conteúdos daquelas variáveis e quebrar a aplicação.
+     * Assim como uma variável global, o padrão Singleton permite que você acesse algum objeto de qualquer lugar no programa. Contudo, ele também protege aquela instância de ser sobrescrita por outro código.
+     * Há outro lado para esse problema: você não quer que o código que resolve o problema #1 fique espalhado por todo seu programa. É muito melhor tê-lo dentro de uma classe, especialmente se o resto do seu código já depende dela.
+     */
+    internal class Singleton
+    {
+        // Adicione um atributo privado estático na classe para o armazenamento da instância singleton.
+        private static Singleton instance;
+
+        // Campo para contar o número de instancia. está sendo declarado como privado para que o usuário não possa muda-lo
+        private int contIns = 0;
+
+        // O construtor é declarado como privado para que somente a classe possa declara-lo. O método getInstance deve ser o único modo de obter o objeto singleton.
+        private Singleton()
+        {
+            // Aumenta o contador de 1 em 1
+            contIns++;
+            // Mostra na tela o contador
+            Console.WriteLine("Número de Instancias: " + contIns);
+            Console.WriteLine("Instancia sendo declarada.");
+        }
+
+        // Declare um método de criação público estático para obter a instância singleton.
+        // A classe Singleton declara o método estático getInstance que retorna a mesma instância de sua própria classe.
+        public static Singleton GetInstance()
+        {
+            // Só permite que uma instancia seja criada
+            if (instance == null)
+            {
+                // Atenção: se você está criando uma aplicação com apoio multithreading (varios processos/aplicaçãoes), você deve colocar um thread lock aqui.
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
---
+++ Anotações/DesignPattern/Design Pattern/Program.cs
@@ -0,0 +1,113 @@
+﻿using Design_Pattern.PatternSingleton;
+
+namespace Design_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual padrão você gostaria de comecar:\n" +
+                "1 - Padrão Sigleton\n" +
+                "2 - Padrão Factory Method\n" +
+                "3 - Padrão Abstract Factory Method\n" +
+                "4 - Padrão Facade\n" +
+                "5 - Padrão Chain of Responsability\n" +
+                "6 - Padrão Template Method\n" +
+                "7 - Padrão Observer");
+            int op = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("##### Padrão Sigleton #####");
+                    Console.ResetColor();
+                    PatternSingleton();
+                    break;
+                case 2:
+                    Console.WriteLine("##### Padrão Factory Method #####");
+                    Console.ResetColor();
+                    PatternFactoryMethod();
+                    break;
+                case 3:
+                    Console.WriteLine("##### Padrão Abstract Factory Method #####");
+                    Console.ResetColor();
+                    PatternAbstractFactoryMethod();
+                    break;
+                case 4:
+                    Console.WriteLine("##### Padrão Facade #####");
+                    Console.ResetColor();
+                    PatternFacade();
+                    break;
+                case 5:
+                    Console.WriteLine("##### Padrão Chain of Responsability #####");
+                    Console.ResetColor();
+                    PatternChainResponsability();
+                    break;
+                case 6:
+                    Console.WriteLine("##### Padrão Template Method #####");
+                    Console.ResetColor();
+                    PatternTemplateMethod();
+                    break;
+                case 7:
+                    Console.WriteLine("##### Padrão Observer #####");
+                    Console.ResetColor();
+                    PatternObserver();
+                    break;
+            }
+        }
+
+        private static void PatternObserver()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternTemplateMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternChainResponsability()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternFacade()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternAbstractFactoryMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternFactoryMethod()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PatternSingleton()
+        {
+            Console.WriteLine();
+            /* O Singleton é um padrão de projeto criacional que permite a você garantir que uma classe tenha apenas uma instância, enquanto provê um ponto de acesso global para essa instância.
+             * Ex: O governo é um excelente exemplo de um padrão Singleton. Um país pode ter apenas um governo oficial. Independentemente das identidades pessoais dos indivíduos que formam governos, o título, “O Governo de X”, é um ponto de acesso global que identifica o grupo de pessoas no command. 
+             */
+            // Tentando criar uma instancia
+            Console.WriteLine("Criando a primeira instancia.");
+            Singleton s1 = Singleton.GetInstance();
+
+            Console.WriteLine();
+
+            // Tentando criar outra instancia
+            // Como não é possivel criar outra instancia, essa instancia não é criada
+            Console.WriteLine("Criando a segunda instancia.");
+            Singleton s2 = Singleton.GetInstance();
+
+            // Como os dois singleton são sempre iguais, sempre cairá no if
+            if (s1 == s2) Console.WriteLine("Existem somente uma instancia.");
+            else Console.WriteLine("Existem instancias diferentes");
+        }
+    }
+}
